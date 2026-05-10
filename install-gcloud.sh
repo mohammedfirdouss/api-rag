f51043263b69c156db7f6cd61ad87730b4c321d5
Mohammed Firdous <mohammedfirdousaraoye@gmail.com>
2026-05-10 09:37:07 +0000

    Add installation script for Google Cloud CLI

---
+++ install-gcloud.sh
@@ -0,0 +1,1 @@
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg && echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list && sudo apt-get update && sudo apt-get install -y google-cloud-cli
